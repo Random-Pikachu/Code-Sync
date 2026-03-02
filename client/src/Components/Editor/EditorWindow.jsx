8a4d5fc783c9a1bcb624fa258e594b7f31d3b302
Sangam Pratap Singh <sangampratapsingh21012006@gmail.com>
2026-03-03 00:53:33 +0530

    refractoring backend and fixes issues with frontent

--- client/src/Components/Editor/EditorWindow.jsx
+++ client/src/Components/Editor/EditorWindow.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useRef, useState } from 'react'
 import * as Monaco from 'monaco-editor'
-import {Editor, useMonaco} from '@monaco-editor/react'
+import { Editor, useMonaco } from '@monaco-editor/react'
 import { initializeSocket } from '../../Connection/socket'
 import { useLocation, useParams } from 'react-router-dom'
 import { inputContext } from '../../Context/CodeInput'
@@ -11,37 +11,37 @@ import toast, { Toaster } from 'react-hot-toast'
 
 
 const EditorWindow = () => {
-    const {languageName} = useContext(inputContext)
+    const { languageName } = useContext(inputContext)
     const socketRef = useRef(null)
 
     const [openTabs, setOpenTabs] = useState([])
     const [activeTabId, setActiveTabId] = useState(null)
-    
+
 
     const editorRef = useRef()
     const monaco = useMonaco()
-    
+
     const [peerPosition, setPeerPosition] = useState({})
     const [cursorDecorations, setCursorDecorations] = useState({})
     const userColorsRef = useRef({})
 
 
-    const {data, setData, fileId, fileStruct, RoomId, setRoomId} = useContext(CodeDataContext)
-    
+    const { data, setData, fileId, fileStruct, RoomId, setRoomId } = useContext(CodeDataContext)
 
 
 
-    const updateCursorDecorations = () =>{
+
+    const updateCursorDecorations = () => {
         if (!editorRef.current) return;
-        
+
         const newDecorationMap = {}
 
         Object.keys(peerPosition).forEach((userName) => {
-            if (userName === location.state.userName) return
+            if (userName === (location.state?.userName || "Anonymous")) return
             const position = peerPosition[userName]
             if (!position) return
 
-            if (!userColorsRef.current[userName]){
+            if (!userColorsRef.current[userName]) {
                 userColorsRef.current[userName] = randomcolor(
                     {
                         luminosity: 'light',
@@ -52,15 +52,15 @@ const EditorWindow = () => {
             const userColor = userColorsRef.current[userName];
 
             const decoration = {
-                range: new monaco.Range(position.lineNumber, position.column, position.lineNumber, position.column+1),
+                range: new monaco.Range(position.lineNumber, position.column, position.lineNumber, position.column + 1),
 
                 options: {
                     className: `cursor-${userName.replace(/\s+/g, "-")}`,
-                    hoverMessage: {value: userName},
+                    hoverMessage: { value: userName },
                     // beforeContentClassName: 'cursor-decoration',
                 }
             }
-            
+
 
             newDecorationMap[userName] = [decoration]
 
@@ -90,7 +90,7 @@ const EditorWindow = () => {
         const styleId = `cursor-${userName.replace(/\s+/g, '-')}`
         let styelEl = document.getElementById(styleId)
 
-        if (!styelEl){
+        if (!styelEl) {
             styelEl = document.createElement('style')
             styelEl.id = styleId
             document.head.appendChild(styelEl)
@@ -129,17 +129,17 @@ const EditorWindow = () => {
         `
     }
 
-        // .cursor-decoration::before {
-        //     content: '${userName}';
-        //     position: absolute;
-        //     width: ${userName.length}+'px';
-        //     color: white;
-        //     height: 26px;
-        //     top: ${topPosition};
-        //     background-color: ${bgColor};
-        //     z-index: z-index: ${1000 + lineNumber + Math.floor(Math.random() * 11)};
-        // }
-    
+    // .cursor-decoration::before {
+    //     content: '${userName}';
+    //     position: absolute;
+    //     width: ${userName.length}+'px';
+    //     color: white;
+    //     height: 26px;
+    //     top: ${topPosition};
+    //     background-color: ${bgColor};
+    //     z-index: z-index: ${1000 + lineNumber + Math.floor(Math.random() * 11)};
+    // }
+
     const hexToRgb = (hex, opacity) => {
         hex = hex.replace(/^#/, "")
         let r = parseInt(hex.substring(0, 2), 16)
@@ -148,7 +148,7 @@ const EditorWindow = () => {
         return `rgba(${r}, ${g}, ${b}, ${opacity})`
     };
 
-    const onMount = (editor) =>{
+    const onMount = (editor) => {
         if (!editor) return;
         editorRef.current = editor
 
@@ -158,81 +158,81 @@ const EditorWindow = () => {
             const position = event.position;
             // console.log("Position of cursor: ", position)
 
-            if (socketRef.current){
+            if (socketRef.current) {
                 // sending cursor position data
                 socketRef.current.emit('cursor-position', {
                     RoomID,
                     position,
-                    userName: location.state.userName
+                    userName: location.state?.userName || "Anonymous"
                 })
             }
-            
+
         })
 
         editor.focus();
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         if (!monaco) return;
-       
-            monaco.editor.defineTheme("Cobalt2", {
-                base: 'vs-dark',
-                inherit: true,
-                rules:[
-                    { token: "comment", foreground: "0088ff", fontStyle: "italic" },
-                    { token: "constant", foreground: "ff628c"},
-                    { token: "entity", foreground: "ffc600"},
-                    { token: "invalid", foreground: "f44542"},
-                    { token: "keyword", foreground: "ff9d00"},
-                    { token: "string", foreground: "a5ff90"},
-                    { token: "variable", foreground: "e1efff"},
-                    { token: "export default", foreground: "ff9d00", fontStyle: "italic"},
-                    
-                ],
-                colors: {
-                    "editor.background": "#193549",
-                }
-            })
 
-            monaco.editor.setTheme("Cobalt2")
-        
-    },[monaco])
+        monaco.editor.defineTheme("Cobalt2", {
+            base: 'vs-dark',
+            inherit: true,
+            rules: [
+                { token: "comment", foreground: "0088ff", fontStyle: "italic" },
+                { token: "constant", foreground: "ff628c" },
+                { token: "entity", foreground: "ffc600" },
+                { token: "invalid", foreground: "f44542" },
+                { token: "keyword", foreground: "ff9d00" },
+                { token: "string", foreground: "a5ff90" },
+                { token: "variable", foreground: "e1efff" },
+                { token: "export default", foreground: "ff9d00", fontStyle: "italic" },
+
+            ],
+            colors: {
+                "editor.background": "#193549",
+            }
+        })
+
+        monaco.editor.setTheme("Cobalt2")
+
+    }, [monaco])
 
     useEffect(() => {
         updateCursorDecorations()
     }, [peerPosition])
 
     const location = useLocation()
-    const {RoomID} = useParams()
+    const { RoomID } = useParams()
 
     useEffect(() => {
         setRoomId(RoomID)
     }, [RoomId])
 
-    
-    
+
+
 
     useEffect(() => {
         const setupSocket = async () => {
             if (socketRef.current) socketRef.current.disconnect()
-            
 
-            
+
+
             socketRef.current = await initializeSocket()
 
             socketRef.current.emit('join', {
                 RoomID,
-                userName: location.state.userName,
+                userName: location.state?.userName || "Anonymous",
             })
 
 
-            socketRef.current.on('joined', ({clients, userName, socketId})=>{
-                
+            socketRef.current.on('joined', ({ clients, userName, socketId }) => {
+
                 // checking ki processed username === current username
                 // if (userName !== location.state.userName){
-                    // console.log(`${userName} joined the room`)
-                    // toast(`${userName} joined the room.`)
-                    
+                // console.log(`${userName} joined the room`)
+                // toast(`${userName} joined the room.`)
+
                 // }
 
                 // giving color to new user
@@ -244,15 +244,15 @@ const EditorWindow = () => {
                 }
 
 
-                
+
             })
 
 
-            socketRef.current.on('code-change', ({value}) => {
+            socketRef.current.on('code-change', ({ value }) => {
                 if (editorRef.current) {
                     const editor = editorRef.current;
                     const currentPosition = editor.getPosition();
-                    
+
                     setData(value)
 
                     setTimeout(() => {
@@ -261,20 +261,20 @@ const EditorWindow = () => {
                         }
                     }, 0);
                 }
-            } )
+            })
 
-            socketRef.current.on('file-open', ({file}) => {
+            socketRef.current.on('file-open', ({ file }) => {
                 setData(file.content)
             })
 
 
-            socketRef.current.on('user-disconnected', ({userName}) => {
+            socketRef.current.on('user-disconnected', ({ userName }) => {
                 // console.log(`${userName} has disconnected`)
                 setPeerPosition((prev) => {
-                    const newPositions = {...prev}
+                    const newPositions = { ...prev }
                     delete newPositions[userName]
                     return newPositions
-                })  
+                })
 
                 const styleId = `cursor-${userName.replace(/\s+/g, '-')}`
                 const styleEl = document.getElementById(styleId)
@@ -282,21 +282,21 @@ const EditorWindow = () => {
                     styleEl.remove()
                 }
 
-                
+
                 if (userColorsRef.current[userName]) {
                     delete userColorsRef.current[userName]
                 }
             })
 
-            socketRef.current.on('cursor-position', ({position, userName}) =>{
+            socketRef.current.on('cursor-position', ({ position, userName }) => {
                 if (!socketRef.current) return;
                 // console.log("received cursor position for ", userName, ": ", position)
                 setPeerPosition((prev) => ({
                     ...prev,
                     [userName]: position
                 }))
-            })  
-            
+            })
+
         };
 
 
@@ -319,52 +319,52 @@ const EditorWindow = () => {
                 }
             })
         }
-        
+
 
     }, [])
 
 
-    
 
-    
 
-  return (
-    <>
-        
-        <div id = 'code-editor'>
-        <Editor
-            // width= "70%" 
-            height="100vh"
-            language={languageName === 'c++' ? 'cpp' : languageName}
-            theme='Cobalt2'
-            onMount={onMount}
 
-            value={data}
-            onChange={(value, event) => {
-                setData(value)
-                // console.log('change: ', value);
-                socketRef.current.emit('code-change', {
-                    RoomID, 
-                    value
-                })      
-                // console.log("RoomID: ", RoomID, "Type: ", typeof(RoomID))
-                socketRef.current.emit('update-file-content', {
-                    RoomID,
-                    fileId,
-                    newContent: value
-                })
-            }}
 
-            options={{
-                fontFamily: "'Cascadia Code', JetBrains Mono, Fira Code",
-                fontSize: 18,
-                fontLigatures: true
-            }}
-        />
-        <Toaster />
-        </div>
-    </>
-  )
+    return (
+        <>
+
+            <div id='code-editor'>
+                <Editor
+                    // width= "70%" 
+                    height="100vh"
+                    language={languageName === 'c++' ? 'cpp' : languageName}
+                    theme='Cobalt2'
+                    onMount={onMount}
+
+                    value={data}
+                    onChange={(value, event) => {
+                        setData(value)
+                        // console.log('change: ', value);
+                        socketRef.current.emit('code-change', {
+                            RoomID,
+                            value
+                        })
+                        // console.log("RoomID: ", RoomID, "Type: ", typeof(RoomID))
+                        socketRef.current.emit('update-file-content', {
+                            RoomID,
+                            fileId,
+                            newContent: value
+                        })
+                    }}
+
+                    options={{
+                        fontFamily: "'Cascadia Code', JetBrains Mono, Fira Code",
+                        fontSize: 18,
+                        fontLigatures: true
+                    }}
+                />
+                <Toaster />
+            </div>
+        </>
+    )
 }
 
 export default EditorWindow
